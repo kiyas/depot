d880eb422b29f9a9d09a24c2359f31cbb8fae187
unknown <kiyasturkmen@gmail.com>
2023-12-23 22:23:09 +0500

    Add Cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
 
   resources :products
+  resources :line_items
+  resources :carts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
