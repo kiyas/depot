76af280a0c158d535f2246ce6afe7be36020caee
Kiyas Haus <kiyasturkmen@gmail.com>
2024-01-01 02:29:40 +0500

    Checkout order

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :products
   resources :line_items
   resources :carts
+  resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
